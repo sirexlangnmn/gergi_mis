e5e325981c31b56b4e8d17f0c2127f12a778ab42
sirexlaangnmn <potolin.federex@gmail.com>
2024-04-22 01:17:51 +0800

    ongoing development of resources page

--- app/src/server.js
+++ app/src/server.js
@@ -57,6 +57,16 @@ app.use(
         ],
     }),
 );
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.gergi.ph');
+    res.setHeader('Access-Control-Allow-Origin', 'https://gergi.ph');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 //app.use(cors()); // Enable All CORS Requests for all origins
 
 app.use(compression()); // Compress all HTTP responses using GZip
